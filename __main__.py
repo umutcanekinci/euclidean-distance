cbeb3f8dac35a37e2944637ed9a2784b430535bd
umutcanekinci <muetnmuetn@gmail.com>
2024-05-28 13:30:34 +0300

    main, eclideanDistance and minDistance functions added

---
+++ __main__.py
@@ -0,0 +1,21 @@
+#
+
+def euclideanDistance(x: int, y: int):
+
+    return (x**2 + y**2)**.5
+
+
+def minDistance(distances: list):
+
+    return min(distances)
+
+def main():
+
+    points = [(1, 1), (2, 2), (3, 3), (4, 4)]
+    distances = [euclideanDistance(x, y) for x, y in points]
+    
+    print("Minimum Euclidean Distance is:", minDistance(distances))
+
+if __name__ == '__main__':
+
+    main()
