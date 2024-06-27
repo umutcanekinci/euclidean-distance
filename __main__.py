5d2780755eaefbdc86312ee1981c4d2122a275dd
umutcanekinci <muetnmuetn@gmail.com>
2024-06-27 20:04:32 +0300

    fix for closing cmd window

--- __main__.py
+++ __main__.py
@@ -13,7 +13,8 @@ def main():
     distances = [euclideanDistance(x, y) for x, y in points]
     
     print("Minimum Euclidean Distance is:", minDistance(distances))
-
+    input("Press Enter to exit...")
+    
 if __name__ == '__main__':
 
     main()
